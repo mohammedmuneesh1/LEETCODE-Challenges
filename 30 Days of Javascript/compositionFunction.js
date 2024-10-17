17cb85431b6b048f6f381bce90f3a5387f77b06d
Mohammed Muneesh E K <mohammedmuneesh1@gmail.com>
2024-10-17 07:40:14 +0530

    composition function , arguement length

--- 30 Days of Javascript/compositionFunction.js
+++ 30 Days of Javascript/compositionFunction.js
@@ -20,6 +20,8 @@ const compose1 = (arr)=>{
     }
 }
 
+// const compose = (arr) => (x) => arr.reduceRight((acc, fn) => fn(acc), x);
+
 /**
  * const fn = compose([x => x + 1, x => 2 * x])
  * fn(4) // 9
